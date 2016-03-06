04f4eaa5be6fbabf63a46d1bef0f283e0084b8da
azb <azbaney@gmail.com>
2016-03-06 14:04:28 -0800

    Upgraded Provide and Request Service Buttons
    The old links have been replaced with new enhanced buttons featuring an
    all new add icon and a stylish CSS background.

--- routes.js
+++ routes.js
@@ -19,7 +19,6 @@ module.exports = function(app, passport) {
     });
 
     app.get('/register', function(req, res) {
-       // res.sendFile('./views/register.html', {root: './'});
        res.render('pages/register.html');
     });       
 
