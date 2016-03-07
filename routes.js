3044a15b9d81beb5f1d255773b7a3e13e85b759c
Hudo Assenco <hudo.assenco@gmail.com>
2016-03-07 13:53:08 -0800

    Fix the temp register page

--- routes.js
+++ routes.js
@@ -19,7 +19,7 @@ module.exports = function(app, passport) {
     });
 
     app.get('/register', function(req, res) {
-        res.sendFile('./views/register.html', {root: './public'});
+        res.render('pages/register.html');
     });       
 
     app.get('/dashboard', requireSession, function(req, res) {
