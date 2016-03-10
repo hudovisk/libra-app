2b873e2bf29239cae3310fcfc269023968d4b964
Pratik Baniya <pratikbaniya@Pratiks-MacBook-Pro.local>
2016-03-09 18:06:52 -0800

    test

--- routes.js
+++ routes.js
@@ -26,6 +26,9 @@ module.exports = function(app, passport) {
         res.render('pages/dashboard.html', {user: req.user});
     });
 
+    app.get('/post', function(req, res) {
+        res.render('pages/post.html', {user: req.user});
+    });
     //...
 
     function requireSession(req, res, next) {
