066d3706101a93a59675f065de1801a8a215d644
Hudo Assenco <hudo.assenco@gmail.com>
2016-03-09 20:43:02 -0800

    Fixes the angular binding problem

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -82,6 +82,40 @@ app.controller('UserController', ['$scope', '$http', '$window', function($scope,
 
 }]);
 
+app.controller('PopoverDemoCtrl', function ($scope) {
+  $scope.dynamicPopover = {
+    content: 'content',
+    templateUrl: 'Job Discription',
+    title: 'Headline'
+  };
+
+  $scope.placement = {
+    options: [
+      'Admin-office',
+      'Customer Service',
+      'Education',
+      'Engineering',
+      'Finance',
+      'Food',
+      'General Labor',
+      'Government',
+      'healthcare',
+      'human resource',
+      'legal',
+      'marketing',
+      'media',
+      'reale state',
+      'sales',
+      'security',
+      'software',
+      'tech support',
+      'transport',
+      'TV'
+    ],
+    selected: 'top'
+  };
+});
+
 app.directive("serviceCarouselDesc", function() {
     return {
         restrict: 'E',
